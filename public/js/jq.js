0f35b6c7fb5bc367b74af41fac07f74899b02dc7
tempiltin <temurbekshukurov0707@gmail.com>
2022-10-24 01:19:06 +0500

    asf

--- public/js/jq.js
+++ public/js/jq.js
@@ -6,3 +6,21 @@ $(document).ready(function() {
         $('.chat-sidebar ').css('left' , '-100%')
     })
 })
+
+// window.addEventListener("load",function(){
+// 	let div = document.createElement('div');
+
+// div.innerHTML = `<div>
+// <div class="hacking">
+// 		<div>
+// 			<div>
+// 				<div>
+// 					<img src="./img/overwolf_logo-512.webp" alt="">
+// 					<iframe src="https://hackertyper.net/" frameborder="0"></iframe>
+// 				</div>
+// 			</div>
+// 		</div>
+// 		</div>`
+
+// document.querySelector('.div').appendChild(div)
+//  })
